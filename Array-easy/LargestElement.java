8096e715f3e54622edea62fe905fdfba3a42601a
SANTHOSH V <145917701+SANTHOSH-VJ@users.noreply.github.com>
2025-10-05 13:17:26 +0530

    Finding LargestElement

---
+++ Array-easy/LargestElement.java
@@ -0,0 +1,52 @@
+//BRUTE FORCE APPROACH
+
+import java.util.*;
+
+class Main {
+    
+    public static void main(String args[]) {
+        
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++) {
+            arr[i]=sc.nextInt();
+        }
+        
+        Arrays.sort(arr);
+            System.out.print(arr[N-1]);
+    }
+}
+/* TC -> O(N log N)
+   Sc -> O(1)
+*/ 
+
+
+//OPTIMAL APPROACH
+
+import java.util.*;
+
+class Main {
+    
+    public static void main(String args[]) {
+        
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++) {
+            arr[i]=sc.nextInt();
+        }
+        int largest=arr[0];
+        
+        for(int i=1;i<N;i++) {
+            if(arr[i]>largest) {
+                largest=arr[i];
+            }
+        }
+        System.out.print(largest);
+    }
+}
+
+/* TC -> O(N)
+   Sc -> O(1)
+*/ 
