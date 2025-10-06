7294afdf6d0623a25bc0b094d8a62936096fa25d
SANTHOSH V <145917701+SANTHOSH-VJ@users.noreply.github.com>
2025-10-06 21:32:09 +0530

    left rotate 1place

---
+++ src/Array-easy/LeftRotateArrayByOnePlace.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+    
+    public static void main(String args[]) {
+        
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        int temp=arr[0];
+        for(int i=1;i<N;i++) {
+            arr[i-1]=arr[i];
+        }
+        arr[N-1]=temp;
+        for(int i=0;i<N;i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+
+/* TC -> O(N)
+   Sc -> O(1)
+*/
