91ff920fae82f24eef004d9a199993a385551178
SANTHOSH V <145917701+SANTHOSH-VJ@users.noreply.github.com>
2025-10-06 20:36:42 +0530

    check array is sorted

---
+++ src/Array-easy/CheckIfArrayIsSorted.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Main {
+    
+    public static void main(String args[]) {
+        
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=1;i<N;i++) {
+            if(arr[i]>arr[N-1]) {
+                System.out.print("The array is sorted");
+            }
+            else{
+                System.out.print("The array is not sorted");
+            }
+        }
