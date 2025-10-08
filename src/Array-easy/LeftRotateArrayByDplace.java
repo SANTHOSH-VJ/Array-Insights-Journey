1c0556aa383a5593a11caaa945c431604bfb632b
SANTHOSH V <145917701+SANTHOSH-VJ@users.noreply.github.com>
2025-10-08 21:36:21 +0530

    optimal rotate by d place

--- src/Array-easy/LeftRotateArrayByDplace.java
+++ src/Array-easy/LeftRotateArrayByDplace.java
@@ -30,3 +30,40 @@ class Main {
 /* TC -> O(N*D)
    Sc -> O(1)
 */
+
+//OPTIMIZED APPROACH
+import java.util.*;
+class Main {
+    
+    public static void reverse(int arr[], int start, int end) {
+        while(start<end) {
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    
+    public static void main(String args[]) {
+        
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++) {
+            arr[i]=sc.nextInt();
+        }
+        int D=sc.nextInt();
+        D=D%N; //in case D>N
+        reverse(arr, 0, D-1);
+        reverse(arr, D, N-1);
+        reverse(arr, 0, N-1);
+        
+        for(int i=0;i<N;i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+/* TC -> O(N)
+   Sc -> O(1)
+*/
